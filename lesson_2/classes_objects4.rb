8211501f04f62aa7ed49c03b29b97747ce84938b
Madz No <madzno12@gmail.com>
2022-03-23 11:27:36 -0400

    adding lesson 2
    ;

---
+++ lesson_2/classes_objects4.rb
@@ -0,0 +1,33 @@
+class Person
+  attr_accessor :last_name, :first_name
+
+  def initialize(full_name)
+    parse_full_name(full_name)
+  end
+
+  def name
+    if last_name.length > 1
+      self.first_name + ' ' + self.last_name
+    else
+      self.first_name
+    end
+  end
+
+  def name=(full_name)
+    parse_full_name(full_name)
+  end
+
+  private
+
+  def parse_full_name(name)
+    first, last = name.split
+    self.first_name = first
+    self.last_name = name.split.length > 1 ? last : ''
+  end
+
+end
+
+bob = Person.new('Robert Smith')
+rob = Person.new('Robert Smith')
+
+p bob.name == rob.name
