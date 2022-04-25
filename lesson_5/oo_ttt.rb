9882aac7578d4d1eace41f336e4376637ab8a778
Madz No <madzno12@gmail.com>
2022-04-25 09:14:36 -0400

    updating oo ttt game

--- lesson_5/oo_ttt.rb
+++ lesson_5/oo_ttt.rb
@@ -1,9 +1,51 @@
-require 'pry'
+module Display
+  def display_welcome_message
+    puts "Welcome to Tic Tac Toe!"
+    puts ''
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Tic Tac Toe! Goodbye!"
+  end
+
+  def display_board
+    puts "You're #{human.marker}. Computer is a #{computer.marker}"
+    puts ""
+    board.draw
+    puts ""
+  end
+
+  def display_result
+    clear_screen_and_display_board
+    case board.winning_marker
+    when TTTGame::HUMAN_MARKER
+      puts "You won!"
+    when TTTGame::COMPUTER_MARKER
+      puts "Computer won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def display_play_again_message
+    puts "Lets play again!"
+    puts ''
+  end
+
+  def clear
+    system 'clear'
+  end
+
+  def clear_screen_and_display_board
+    clear
+    display_board
+  end
+end
 
 class Board
-  WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7,8,9]] + #rows
-                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + #columns
-                  [[1, 5, 9], [3, 5, 7]] #diagonals
+  WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                  [[1, 5, 9], [3, 5, 7]] # diagonals
 
   def initialize
     @squares = {}
@@ -58,7 +100,7 @@ class Board
 
   def three_identical_markers?(squares)
     markers = squares.select(&:marked?).collect(&:marker)
-    return false if markers.size !=3
+    return false if markers.size != 3
     markers.min == markers.max
   end
 end
@@ -94,8 +136,11 @@ class Player
 end
 
 class TTTGame
+  include Display
+
   HUMAN_MARKER = 'X'
   COMPUTER_MARKER = 'O'
+  FIRST_TO_MOVE = HUMAN_MARKER
 
   attr_reader :board, :human, :computer
 
@@ -103,23 +148,32 @@ class TTTGame
     @board = Board.new
     @human = Player.new(HUMAN_MARKER)
     @computer = Player.new(COMPUTER_MARKER)
+    @current_marker = FIRST_TO_MOVE
   end
 
-  def display_welcome_message
-    puts "Welcome to Tic Tac Toe!"
-    puts ''
+  def play
+    clear
+    display_welcome_message
+
+    loop do
+      display_board
+
+      loop do
+        current_player_moves
+        break if board.someone_won? || board.full?
+        clear_screen_and_display_board if human_turn?
+      end
+
+      display_result
+      break unless play_again?
+      reset
+      display_play_again_message
+    end
+
+    display_goodbye_message
   end
 
-  def display_goodbye_message
-    puts "Thanks for playing Tic Tac Toe! Goodbye!"
-  end
-
-  def display_board
-    puts "You're #{human.marker}. Computer is a #{computer.marker}"
-    puts ""
-    board.draw
-    puts ""
-  end
+  private
 
   def human_moves
     puts "Chose a square (#{board.unmarked_keys.join(', ')}): "
@@ -138,18 +192,6 @@ class TTTGame
     board[board.unmarked_keys.sample] = computer.marker
   end
 
-  def display_result
-    clear_screen_and_display_board
-    case board.winning_marker
-    when HUMAN_MARKER
-      puts "You won!"
-    when COMPUTER_MARKER
-      puts "Computer won!"
-    else
-      puts "It's a tie!"
-    end
-  end
-
   def play_again?
     answer = nil
     loop do
@@ -162,48 +204,24 @@ class TTTGame
     answer == 'y'
   end
 
-  def clear
-    system 'clear'
-  end
-
-  def clear_screen_and_display_board
-    clear
-    display_board
-  end
-
   def reset
     board.reset
+    @current_marker = FIRST_TO_MOVE
     clear
   end
 
-  def display_play_again_message
-    puts "Lets play again!"
-    puts ''
+  def human_turn?
+    @current_marker == HUMAN_MARKER
   end
 
-  def play
-    clear
-    display_welcome_message
-
-    loop do
-      display_board
-
-      loop do
-        human_moves
-        break if board.someone_won? || board.full?
-
-        computer_moves
-        break if board.someone_won? || board.full?
-
-        clear_screen_and_display_board
-      end
-      display_result
-      break unless play_again?
-      reset
-      display_play_again_message
+  def current_player_moves
+    if human_turn?
+      human_moves
+      @current_marker = COMPUTER_MARKER
+    else
+      computer_moves
+      @current_marker = HUMAN_MARKER
     end
-
-    display_goodbye_message
   end
 end
 
