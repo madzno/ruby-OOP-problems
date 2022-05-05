0be53bb5101a196fbe5d69d7ed16e617cc80f9cf
Madz No <madzno12@gmail.com>
2022-05-05 16:29:21 -0400

    updating debugging exercises

---
+++ debugging/animal_kingdom.rb
@@ -0,0 +1,54 @@
+class Animal
+  def initialize(diet, superpower)
+    @diet = diet
+    @superpower = superpower
+  end
+
+  def move
+    puts "I'm moving!"
+  end
+
+  def superpower
+    puts "I can #{@superpower}!"
+  end
+end
+
+class Fish < Animal
+  def move
+    puts "I'm swimming!"
+  end
+end
+
+class Bird < Animal
+end
+
+class FlightlessBird < Bird
+  def initialize(diet, superpower)
+    super
+  end
+
+  def move
+    puts "I'm running!"
+  end
+end
+
+class SongBird < Bird
+  def initialize(diet, superpower, song)
+    super(diet, superpower)
+    @song = song
+  end
+
+  def move
+    puts "I'm flying!"
+  end
+end
+
+# Examples
+
+unicornfish = Fish.new(:herbivore, 'breathe underwater')
+penguin = FlightlessBird.new(:carnivore, 'drink sea water')
+robin = SongBird.new(:omnivore, 'sing', 'chirp chirrr chirp chirp chirrrr')
+
+# Argument error for initialize method, given 3 arguments and only expecting 2
+# this is due to SongBird's initialize method taking three arguments while its
+# superclass Animal's initialize method only takes 2 argumen
